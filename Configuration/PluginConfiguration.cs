1da02d0cc2b04fd91406c9ccab2423c01706a807
Mix1C <asdasfafsasfasfsaf@gmail.com>
2026-03-06 13:43:31 -0500

    Create PluginConfiguration.cs

---
+++ Configuration/PluginConfiguration.cs
@@ -0,0 +1,8 @@
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.LogManager.Configuration;
+
+public class PluginConfiguration : BasePluginConfiguration
+{
+    // No configuration needed for now
+}
